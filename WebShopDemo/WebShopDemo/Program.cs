c0c9e87b814ee6739fd4661ac1baa4b47fd2ed26
donetian-petkov <donetian+petkov@gmail.com>
2022-11-26 14:15:37 +0200

    Initial Migration Entity Framework

--- WebShopDemo/WebShopDemo/Program.cs
+++ WebShopDemo/WebShopDemo/Program.cs
@@ -7,9 +7,9 @@ using WebShopDemo.Core.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
