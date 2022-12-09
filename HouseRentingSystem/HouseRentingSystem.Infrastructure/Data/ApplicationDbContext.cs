05a17c88e0e7844949a2b5aff120047b72a886b1
donetian-petkov <donetian+petkov@gmail.com>
2022-12-09 09:40:52 +0200

    Seeding Database

--- HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HouseRentingSystem.Infrastructure.Data.Configuration;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Infrastructure.Data;
@@ -8,5 +10,23 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        builder.ApplyConfiguration(new UserConfiguration());
+        builder.ApplyConfiguration(new AgentConfiguration());
+        builder.ApplyConfiguration(new CategoryConfiguration());
+        builder.ApplyConfiguration(new HouseConfiguration());
+
+        base.OnModelCreating(builder);
+    }
+    
+    public DbSet<House> Houses { get; set; }
+    
+    public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<Agent> Agents { get; set; }
 }
