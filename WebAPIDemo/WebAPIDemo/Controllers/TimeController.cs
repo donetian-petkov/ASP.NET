d523da5fb27b8c9dd32b0abf8e29391e56ad73a8
donetian-petkov <donetian+petkov@gmail.com>
2022-12-10 11:58:04 +0200

    WebAPI + jQuery

---
+++ WebAPIDemo/WebAPIDemo/Controllers/TimeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult GetCurrentTime()
+        {
+            return Ok(new { currentTime = DateTime.Now.ToLocalTime() });
+        }
+    }
+}
